e98340a4a7212b699df1d5b0ed0ff5de457c8504
Noah Zarro <zarron@student.ethz.ch>
2018-10-30 18:53:10 +0100

    actualStep, DesiredDirection and plot implemented

--- code/Main.py
+++ code/Main.py
@@ -1,25 +1,37 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from DesiredDirection import desiredDirection
+from DesiredDirection import desired_direction
+from actualStep import actual_step
+import matplotlib.pyplot as plt
+import matplotlib.path as mpath
+import matplotlib.patches as mpatches
 
 # Define Ant
-position = np.array([0,0])
-globalVector = np.array([10,10])
+position = (0,0)
+globalVector = (10,10)
 stepLength = 1
-track = position
+track = list()
+track.append(position)
 
 cycles = 100
 
-# Define Map
+# Define Map (0 desert, 1 channel, 2 channel entry/exit, 3 home, 4 object)
 
 
 # Main Loop
 
 for i in range(cycles):
-    desired = desiredDirection(globalVector, position)
-    possible = possibleDirection(position)
-    step = actualStep(desired,possible,stepLength)
+    desired = desired_direction(globalVector, position)
+    possible = possible_direction(position)
+    step = actual_step(desired, possible, stepLength)
     position = position + step
-    track = np.append(track,position)
+    globalVector = globalVector - step
+    track.append(position)
 
 #plot
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+path = mpath.Path(track)
+patch = mpatches.PathPatch(path)
+ax.add_patch(patch)
---
+++ code/actualStep.py
@@ -0,0 +1,24 @@
+import math
+
+def actual_step(desired, possible, stepLength):
+    #returns actual step Vector normalized to stepLength
+    xmin = possible(0)
+    xmax = possible(1)
+    ymin = possible(2)
+    ymax = possible(3)
+
+    actual = desired
+
+    if (desired[0]<0 and xmin > -1): #if desired.x < 0 and xmin not -1 (xmin == 0) set actual.x to 0
+        actual[0] = 0
+
+    if (desired[0]>0 and xmax < 1): #if x positive but not possible, set x to 0
+        actual[0] = 0
+
+    if (desired[1]<0 and ymin > -1): #if desired.y < 0 and ymin not -1 (xmin == 0) set actual.y to 0
+        actual[1] = 0
+
+    if (desired[1]>0 and ymax < 1): #if y positive but not possible, set y to 0
+        actual[1] = 0
+
+    return actual/math.sqrt((actual[0])^2+(actual[1])^2)
