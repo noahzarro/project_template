c9f347ba30fdf8be626d65fb6e31be5f9a2daf9e
JosuaGraf <grafjo@student.ethz.ch>
2018-10-30 18:55:50 +0100

    possible_Diraction move for desert implemented

--- code/Main.py
+++ code/Main.py
@@ -1,9 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from DesiredDirection import desiredDirection
+from possibleDirection import possible_direction
 
 # Define Ant
-position = np.array([0,0])
+position = np.array([0,0])      #possible values: floats between 0 and 1600
 globalVector = np.array([10,10])
 stepLength = 1
 track = position
@@ -12,14 +13,19 @@ cycles = 100
 
 # Define Map
 map = np.zeros((1600,1600))
-map[0,0:799] = 1    #Define channel
-map[800,800] = 3    #Define nest
+map[0,:] = 1        #Define border
+map[1599,:] = 1
+map[:,0] = 1
+map[:,1599] = 1
+map[1:4,1:797] = 1  #Define channel of width 3
+map[1:4,798] = 2    #Define channelexit
+map[799:800,799:800] = 3    #Define nest of size 2x2
 
 # Main Loop
 
 for i in range(cycles):
     desired = desiredDirection(globalVector, position)
-    possible = possibleDirection(position)
+    possible = possible_direction(position)
     step = actualStep(desired,possible,stepLength)
     position = position + step
     track = np.append(track,position)
--- code/possibleDirection.py
+++ code/possibleDirection.py
@@ -1,1 +1,29 @@
-def possibleDirection(position):
+import numpy as np
+import math as m
+
+
+def possible_direction(position):
+    restriction = np.zeros(4)       # empty vector for return [x_min,x_max,y_min,y_max]
+    square = np.array([m.floor(position[0]), m.floor(position[1])])      # descretise the position to a square
+    if map(square) == 0:
+        if map(square + np.array([-1, -1])) == (0 or 2):
+            restriction[0] = -1     # x_min = -1
+            restriction[2] = -1     # y_min = -1
+        if map(square + np.array([-1, 1])) == (0 or 2):
+            restriction[0] = -1     # x_min = -1
+            restriction[3] = 1      # y_max = 1
+        if map(square + np.array([1, -1])) == (0 or 2):
+            restriction[1] = 1      # x_max = 1
+            restriction[2] = -1     # y_min = -1
+        if map(square + np.array([1, 1])) == (0 or 2):
+            restriction[1] = 1      # x_max = 1
+            restriction[3] = 1      # y_max = 1
+
+
+
+
+
+
+
+
+
